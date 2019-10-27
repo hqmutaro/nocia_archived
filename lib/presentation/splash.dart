a8cdd7786634a9340369c01b92d68fe4ed38336a
github nitf <2tf2003@gmail.com>
2019-10-28 01:12:55 +0900

    add: SplashScreen

---
+++ lib/presentation/splash.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:nocia/presentation/news/main.dart';
+import 'package:nocia/presentation/home.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class Splash extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 3,
+      navigateAfterSeconds: Test(),
+      title: Text("Nocia"),
+      backgroundColor: Colors.white,
+      loaderColor: Colors.white
+    );
+  }
+}
