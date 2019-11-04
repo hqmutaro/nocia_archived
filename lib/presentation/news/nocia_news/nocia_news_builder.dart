664bcfefcf7dfb60c836cbe772e8d5219263f7cc
github nitf <2tf2003@gmail.com>
2019-11-04 16:43:38 +0900

    add: NociaNewsBuilder

---
+++ lib/presentation/news/nocia_news/nocia_news_builder.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nocia/infrastructure/repository/firebase_news_repository.dart';
+import 'package:nocia/presentation/news/bloc.dart';
+import 'package:nocia/presentation/news/nocia_news/nocia_news_tile.dart';
+
+class NociaNewsBuilder extends StatelessWidget {
+
+  NociaNewsBuilder();
+
+  @override
+  Widget build(BuildContext context) {
+    final _newsBloc = BlocProvider.of<NewsBloc>(context);
+    _newsBloc.add(UpdateNociaNews());
+    return BlocBuilder<NewsBloc, NewsStream>(
+      bloc: _newsBloc,
+      builder: (context, stream) {
+        var repository = stream.repository as FirebaseNewsRepository;
+        if (repository == null) {
+          return Center(child: CircularProgressIndicator());
+        }
+        return FutureBuilder<List<dynamic>>(
+          future: repository.getNews(),
+          builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+            print(snapshot.data);
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            }
+            var widgets = <Widget>[];
+            snapshot.data.forEach((data) {
+              if (!(snapshot.data.indexOf(data) >= 5)) {
+                widgets.add(getCard(context, data));
+              }
+            });
+            return Column(children: widgets);
+          }
+        );
+      }
+    );
+  }
+}
