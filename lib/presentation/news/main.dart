64e45a848bb138e603a34543d9592d288c5916c9
github nitf <2tf2003@gmail.com>
2019-11-04 16:46:24 +0900

    feat: BLoCに対応

--- lib/presentation/news/main.dart
+++ lib/presentation/news/main.dart
@@ -1,5 +1,18 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:nocia/presentation/nocia.dart';
+import 'package:flutter/material.dart' as prefix0;
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+import 'package:nocia/infrastructure/repository/firebase_news_repository.dart';
+import 'package:nocia/presentation/news/bloc.dart';
+import 'package:nocia/presentation/news/nocia_news/nocia_news_view.dart';
+import 'package:nocia/presentation/news/school_news/school_news_builder.dart';
+import 'package:nocia/presentation/news/school_news/school_news_view.dart';
+import 'package:nocia/presentation/nocia_theme.dart';
+import 'package:nocia/presentation/ui/header/wave_clipper_header.dart';
+import 'package:webfeed/domain/rss_item.dart';
+
+import 'nocia_news/nocia_news_builder.dart';
 
 
 class News extends StatefulWidget {
@@ -15,32 +28,58 @@ class _News extends State<News> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Nocia.getAppBar("ニュース"),
-      drawer: Nocia.getDrawer(),
-      backgroundColor: Color(0xFFEFEFEF),
-      body: Column(
-        children: <Widget>[
+      appBar: AppBar(
+        title: Text("ニュース"),
+        centerTitle: true,
+        actions: <Widget>[
           Padding(
-              padding: const EdgeInsets.only(top: 10, left: 0),
-              child: Text(
-                  "新着情報",
-                  style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold
-                  )
-              )
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10, left: 20),
-            child: Text(
-                "学校の活動",
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold
-                )
-            )
+            padding: EdgeInsets.all(10),
+            child: Icon(Icons.settings),
           )
         ],
+      ),
+      drawer: Drawer(
+          child: ListView(
+            children: <Widget> [
+              DrawerHeader(
+                  decoration: BoxDecoration(
+                      color: Colors.indigo
+                  ),
+                  child: null
+              ),
+              ListTile(
+                title: Text('First Menu Item'),
+                onTap: () {},
+              ),
+              ListTile(
+                title: Text('Second Menu Item'),
+                onTap: () {},
+              ),
+              Divider(),
+              ListTile(
+                title: Text('About'),
+                onTap: () {},
+              ),
+            ],
+          )
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 10),
+            WaveClipperHeader(message: "新着情報"),
+            BlocProvider<NewsBloc>(
+              builder: (BuildContext context) => NewsBloc(),
+              child: NociaNewsBuilder(),
+            ),
+            WaveClipperHeader(message: "学校の活動"),
+            BlocProvider<NewsBloc>(
+              builder: (BuildContext context) => NewsBloc(),
+              child: SchoolNewsBuilder(),
+            ),
+          ],
+        )
       )
     );
   }
