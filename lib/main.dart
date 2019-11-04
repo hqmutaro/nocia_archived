caf2b2510ddb53fdc7908716ae7bd002a5481bc5
github nitf <2tf2003@gmail.com>
2019-11-04 16:43:13 +0900

    feat: set widget theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:nocia/domain/department.dart';
-import 'package:nocia/domain/term.dart';
-import 'package:nocia/infrastructure/repository/server_subject_list_repository.dart';
 import 'package:nocia/presentation/app.dart';
 
 void main() async{
   runApp(App());
-  var repository = ServerSubjectListRepository();
-  var subjectList = await repository.subjectList(Department.MEDIA_INFORMATION_ENGINEERING, 1, Term.First);
-  print(subjectList);
 }
