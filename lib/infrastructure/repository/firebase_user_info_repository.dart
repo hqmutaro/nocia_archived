8114a6d9827f0ecd99c18a788e3282f38f5dade7
github nitf <2tf2003@gmail.com>
2019-11-11 10:54:51 +0900

    add: UserInfoRepository

---
+++ lib/infrastructure/repository/firebase_user_info_repository.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:nocia/domain/repository/user_info_repository.dart';
+import 'package:nocia/infrastructure/repository/db_reference.dart';
+
+class FirebaseUserInfoRepository implements UserInfoRepository {
+  final FirebaseUser user;
+
+  FirebaseUserInfoRepository({@required this.user});
+
+  Future<void> setUpUserData({String name}) async{
+    await instance().child("user").child(user.uid).set(<String,dynamic>{
+      "name": name,
+      "e-mail": user.email,
+    });
+  }
+
+  Future<bool> existsUserInfo() async{
+    var userInfo = await getUserInfo();
+    return userInfo != null;
+  }
+
+  Future<dynamic> getUserInfo() async{
+    var data = await instance().child("user").child(user.uid).once().then((data) => data.value);
+    return data;
+  }
+}
