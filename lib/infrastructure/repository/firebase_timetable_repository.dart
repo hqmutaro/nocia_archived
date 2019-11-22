0f8709d61900004d66ac99a9073c9248832c16cf
github nitf <2tf2003@gmail.com>
2019-11-22 10:30:41 +0900

    feat: initialize timetable

--- lib/infrastructure/repository/firebase_timetable_repository.dart
+++ lib/infrastructure/repository/firebase_timetable_repository.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:nocia/domain/department.dart';
-import 'package:nocia/domain/lecture.dart';
-import 'package:nocia/domain/repository/subject_data_repository.dart';
-import 'package:nocia/domain/term.dart';
-import 'package:nocia/infrastructure/repository/dto/lecture_dto.dart';
+import 'package:nocia/domain/repository/timetable_repository.dart';
+import 'package:nocia/infrastructure/repository/db_reference.dart';
 import 'package:nocia/infrastructure/repository/firebase_user_info_repository.dart';
-import 'package:nocia/infrastructure/repository/server_subject_data_repository.dart';
 import 'package:nocia/infrastructure/repository/user_repository.dart';
 
-class FirebaseTimetableRepository {
+class FirebaseTimetableRepository extends TimetableRepository{
   final String uid;
 
   FirebaseTimetableRepository({@required this.uid}): assert(uid != null);
+
+  Future<dynamic> createTimetable() async{
+    var timetables = <List<Map<String, dynamic>>>[];
+    var timeColumn;
+    var range = List<int>.generate(5, (i) => i + 1);
+    range.forEach((time) {
+      timeColumn = <Map<String, dynamic>>[];
+      range.forEach((day) {
+        timeColumn.add(<String, dynamic>{
+          "cell": day + ((time - 1) * 5),
+          "grade": null,
+          "id": null,
+          "name": null,
+          "staffs": [],
+          "term": null
+        });
+      });
+      timetables.add(timeColumn);
+    });
+    print(timetables);
+    await instance().child("user").child(uid).child("timetable").set(timetables);
+    return timetables;
+  }
 
   Future<dynamic> lectureTimetables() async{
     var user = await UserRepository().getUser();
@@ -19,6 +38,9 @@ class FirebaseTimetableRepository {
 
     var dayTimetable = <List<Map<String, dynamic>>>[];
     var timetableList = info["timetable"];
+    if (timetableList == null) {
+      timetableList = await createTimetable(); // Initialize Timetable
+    }
     for (var list in timetableList) {
       var timetable = <Map<String, dynamic>>[];
 
