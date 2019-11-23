f6f3afb378b4896dffa09e41a66e9450e1512cb5
github nitf <2tf2003@gmail.com>
2019-11-24 03:23:13 +0900

    fix: change background color

--- lib/presentation/ui/drawer/nocia_drawer.dart
+++ lib/presentation/ui/drawer/nocia_drawer.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nocia/application/user/user_bloc.dart';
+import 'package:nocia/domain/user.dart';
 import 'package:nocia/infrastructure/repository/firebase_user_info_repository.dart';
 import 'package:nocia/infrastructure/service/service_email_auth.dart';
 import 'package:nocia/infrastructure/service/service_google_auth.dart';
@@ -11,51 +14,51 @@ import 'package:nocia/infrastructure/service/firebase_auth.dart';
 
 class NociaDrawer extends StatelessWidget {
 
-  final FirebaseUser user;
+  final User user;
 
   NociaDrawer({this.user});
 
   @override
   Widget build(BuildContext context) {
-    var userRepository = FirebaseUserInfoRepository(user: user);
-    return FutureBuilder(
-      future: userRepository.existsUserInfo(),
-      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-        if (snapshot.hasData) {
-          return getDrawer(context, user.displayName, user.photoUrl);
-        }
-        return Container();
-      },
-    );
+    return getDrawer(context, user.name, user.photoUrl);
   }
 
   Drawer getDrawer(BuildContext context, String displayName, String photoUrl) {
     return Drawer(
         child: ListView(
           children: <Widget> [
-            UserAccountsDrawerHeader(
-              accountName: Text(displayName),
-              accountEmail: Text(""),
-              decoration: BoxDecoration(
-                  color: Color.fromRGBO(58, 66, 86, 1.0)
-              ),
-              currentAccountPicture: CircleAvatar(
-                  backgroundColor: Colors.white,
-                  backgroundImage: NetworkImage(photoUrl ?? "http://freeiconbox.com/icon/256/17004.png")
+            Container(
+              color: Colors.white,
+              child: UserAccountsDrawerHeader(
+                accountName: Text(displayName),
+                accountEmail: Text(""),
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(58, 66, 86, 1.0)
+                ),
+                currentAccountPicture: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    backgroundImage: NetworkImage(photoUrl ?? "http://freeiconbox.com/icon/256/17004.png")
+                ),
               ),
             ),
             ListTile(
               leading: Icon(Icons.home),
               title: Text('ホーム',style:  TextStyle(color: Colors.black)),
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Home(user: user)));
+                Navigator.push(context, MaterialPageRoute(builder: (context) => BlocProvider<UserBloc>(
+                  builder: (BuildContext context) => UserBloc(),
+                  child: Home(),
+                )));
               },
             ),
             ListTile(
               leading: Icon(Icons.settings),
               title: Text('設定',style:  TextStyle(color: Colors.black)),
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Config()));
+                Navigator.push(context, MaterialPageRoute(builder: (context) => BlocProvider<UserBloc>(
+                  builder: (BuildContext context) => UserBloc(),
+                  child: Config(),
+                )));
               },
             ),
             Divider(),
