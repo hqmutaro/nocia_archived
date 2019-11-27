eb5c038e816e1d68ebc9dd595647d3220c730f0e
github nitf <2tf2003@gmail.com>
2019-11-27 12:46:39 +0900

    feat: add any user data

--- lib/application/user/user_bloc.dart
+++ lib/application/user/user_bloc.dart
@@ -3,8 +3,11 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:nocia/application/user/user_event.dart';
 import 'package:nocia/application/user/user_state.dart';
+import 'package:nocia/domain/department.dart';
+import 'package:nocia/domain/term.dart';
 import 'package:nocia/domain/user.dart';
 import 'package:nocia/infrastructure/repository/firebase_timetable_repository.dart';
+import 'package:nocia/infrastructure/repository/firebase_user_info_repository.dart';
 import 'package:nocia/infrastructure/repository/user_repository.dart';
 
 class UserBloc extends Bloc<UserEvent, UserState> {
@@ -19,12 +22,22 @@ class UserBloc extends Bloc<UserEvent, UserState> {
       var firebaseUser = await repository.getUser();
       var timetableRepository = FirebaseTimetableRepository(uid: firebaseUser.uid);
       var timetable = await timetableRepository.lectureTimetables();
+      var userInfoRepository = FirebaseUserInfoRepository(user: firebaseUser);
+      var userData = await userInfoRepository.getUserInfo();
+      print("data $userData");
+      print(getCourse(userData["course"]));
+
       var user = User(
           firebaseUser: firebaseUser,
           name: firebaseUser.displayName,
           photoUrl: firebaseUser.photoUrl,
+          department: getDepartment(userData["department"]),
+          course: getCourse(userData["course"]),
+          grade: userData["grade"],
+          term: getTerm(userData["term"]),
           timetable: timetable
       );
+      print("suss $user");
       yield UserStream(user: user);
     }
 
--- lib/domain/user.dart
+++ lib/domain/user.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:nocia/domain/department.dart';
+import 'package:nocia/domain/term.dart';
 import 'package:nocia/infrastructure/repository/firebase_timetable_repository.dart';
 import 'package:nocia/infrastructure/service/service_email_auth.dart';
 import 'package:nocia/infrastructure/service/service_google_auth.dart';
@@ -9,17 +11,28 @@ class User {
   final FirebaseUser firebaseUser;
   final String name;
   final String photoUrl;
+  final Department department;
+  final Course course;
+  final int grade;
+  final Term term;
   dynamic timetable;
 
   User({
     @required this.firebaseUser,
     @required this.name,
     @required this.photoUrl,
+    @required this.department,
+    @required this.course,
+    @required this.grade,
+    @required this.term,
     @required this.timetable
   }):
       assert(firebaseUser != null),
       assert(name != null),
       assert(photoUrl != null),
+      assert(department != null),
+      assert(grade != null),
+      assert(term != null),
       assert(timetable != null);
 
   Future<void> signOut() async{
