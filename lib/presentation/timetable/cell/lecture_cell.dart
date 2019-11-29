7dbe311a3d922585c3c6ce73fcf5edb276d59c61
github nitf <2tf2003@gmail.com>
2019-11-29 10:31:37 +0900

    feat: 待機時はセルの色を変更

--- lib/presentation/timetable/cell/lecture_cell.dart
+++ lib/presentation/timetable/cell/lecture_cell.dart
@@ -3,7 +3,9 @@ import 'package:nocia/domain/department.dart';
 import 'package:nocia/domain/term.dart';
 import 'package:nocia/infrastructure/repository/dto/lecture_dto.dart';
 import 'package:nocia/infrastructure/repository/firebase_timetable_repository.dart';
+import 'package:nocia/infrastructure/repository/firebase_user_info_repository.dart';
 import 'package:nocia/infrastructure/repository/server_subject_data_repository.dart';
+import 'package:nocia/infrastructure/repository/user_repository.dart';
 
 import '../main.dart';
 
@@ -11,11 +13,13 @@ class LectureCell extends StatefulWidget {
   final int cell;
   final Map<dynamic, dynamic> lecture;
   final String uid;
+  final String selectLecture;
 
   LectureCell(
       this.cell,
       this.lecture,
-      this.uid
+      this.uid,
+      this.selectLecture
       ):  assert(cell != null),
         assert(lecture != null),
         assert(uid != null);
@@ -26,11 +30,13 @@ class LectureCell extends StatefulWidget {
 
 class _LectureCell extends State<LectureCell> {
   Map<dynamic, dynamic> lecture;
+  Color defaultColor;
 
   @override
   void initState() {
     super.initState();
     this.lecture = widget.lecture;
+    this.defaultColor = Color(0xFFEFEFEF);
   }
 
   @override
@@ -76,8 +82,8 @@ class _LectureCell extends State<LectureCell> {
                   )
               ),
               decoration: BoxDecoration(
-                border: Border(left: BorderSide(width: 4.0, color: subject == null ? Color(0xFFEFEFEF) : Colors.blue)),
-                color: subject == null ? const Color(0xFFEFEFEF) : Color(0xFFBAE3F9),
+                border: Border(left: BorderSide(width: 4.0, color: subject == null ? this.defaultColor : Colors.blue)),
+                color: subject == null ? this.defaultColor : Color(0xFFBAE3F9),
               ),
               height: 150,
               width: 60,
@@ -137,29 +143,57 @@ class _LectureCell extends State<LectureCell> {
         }
       },
       onDoubleTap: () async{
-        var repository = FirebaseTimetableRepository(uid: widget.uid);
-        var data = await getSubjectData(Timetable.lectureValue);
-        var map = <String, dynamic>{
-          "name": data["name"],
-          "id": data["id"],
-          "staffs": data["staffs"],
-          "grade": data["classes"][0]["grade"],
-          "term": data["classes"][0]["term"]
-        };
-        var lecture = LectureDTO.decode(map);
-        setState(() {
-          this.lecture = map;
-        });
-        await repository.setLecture(widget.cell, lecture);
+        if (subject == null) {
+          setState(() => this.defaultColor = Colors.deepOrangeAccent);
+          var repository = FirebaseTimetableRepository(uid: widget.uid);
+          var data = await getSubjectData(widget.selectLecture);
+          var map = <String, dynamic>{
+            "name": data["name"],
+            "id": data["id"],
+            "staffs": data["staffs"],
+            "grade": data["classes"][0]["grade"],
+            "term": data["classes"][0]["term"]
+          };
+          var lecture = LectureDTO.decode(map);
+          setState(() {
+            this.defaultColor = Color(0xFFEFEFEF);
+            this.lecture = map;
+          });
+          await repository.setLecture(widget.cell, lecture);
+        }
       },
-      onLongPress: () {
+      onLongPress: () async{
+        if (subject != null) {
+          var repository = FirebaseTimetableRepository(uid: widget.uid);
+          await repository.deleteLecture(widget.cell);
+          var map = <String, dynamic>{
+            "name": null,
+            "id": null,
+            "staffs": null,
+            "grade": null,
+            "term": null
+          };
+          setState(() {
+            this.lecture = map;
+          });
+        }
       },
     );
   }
 
   Future<dynamic> getSubjectData(String subject) async{
-    var repository = ServerSubjectDataRepository();
-    var subjectData = repository.subjectData(subject, Department.MEDIA_INFORMATION_ENGINEERING, 5, Term.Final);
+    var user = await UserRepository().getUser();
+    var userInfoRepository = FirebaseUserInfoRepository(user: user);
+    var data = await userInfoRepository.getUserInfo();
+    var subjectDataRepository = ServerSubjectDataRepository();
+    var department = getDepartment(data["department"]);
+    var subjectData = subjectDataRepository.subjectData(
+        subject,
+        department,
+        data["grade"],
+        getTerm(data["term"]),
+        course: department == Department.ADVANCED ? getCourse((data["course"])) : null
+    );
     return subjectData;
   }
 }
