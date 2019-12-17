9e5335e4882221a180fc3c948f4fdb01b52fe2fc
github nitf <2tf2003@gmail.com>
2019-12-17 17:54:06 +0900

    feat: ニュースのジャンルを選択させる

--- lib/domain/repository/rss_repository.dart
+++ lib/domain/repository/rss_repository.dart
@@ -1,7 +1,8 @@
+import 'package:nocia/infrastructure/repository/server_rss_repository.dart';
 import 'package:webfeed/domain/rss_feed.dart';
 import 'package:webfeed/domain/rss_item.dart';
 
 abstract class RSSRepository {
-  Future<List<RssItem>> getItems();
+  Future<List<RssItem>> getItems(NewsType type);
   Future<RssFeed> getFeed(String url);
 }
