4a999c4ee091ef959413e50d6db2074976b12e3c
github nitf <2tf2003@gmail.com>
2020-01-02 23:33:26 +0900

    サインアウトしない

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:flutter/material.dart';
+import 'package:nocia/infrastructure/repository/firestore_reference.dart';
+import 'package:nocia/infrastructure/repository/user_repository.dart';
 import 'package:nocia/presentation/app.dart';
 
 void main() async{
+  //await auth.FirebaseAuth.instance.signOut();
   runApp(App());
 }
